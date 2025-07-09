eef906befd20ca71643903fc21fd688bf9a06ea5
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-09 20:24:58 +0700

    Update sudo-xorg.sh

--- sudo-xorg.sh
+++ sudo-xorg.sh
@@ -4,20 +4,30 @@ Defaults env_keep += XORG_PREFIX
 Defaults env_keep += XORG_CONFIG
 EOF
 
-cat >> /etc/profile.d/xorg.sh << "EOF"
+sudo bash -c cat > /etc/profile.d/xorg.sh << EOF
+# Menambahkan direktori ke PATH
+export PATH=$XORG_PREFIX/bin:$PATH
 
-pathappend $XORG_PREFIX/bin             PATH
-pathappend $XORG_PREFIX/lib/pkgconfig   PKG_CONFIG_PATH
-pathappend $XORG_PREFIX/share/pkgconfig PKG_CONFIG_PATH
+# Menambahkan direktori ke PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=$XORG_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=$XORG_PREFIX/share/pkgconfig:$PKG_CONFIG_PATH
 
-pathappend $XORG_PREFIX/lib             LIBRARY_PATH
-pathappend $XORG_PREFIX/include         C_INCLUDE_PATH
-pathappend $XORG_PREFIX/include         CPLUS_INCLUDE_PATH
+# Menambahkan direktori ke LIBRARY_PATH
+export LIBRARY_PATH=$XORG_PREFIX/lib:$LIBRARY_PATH
 
-ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
+# Menambahkan direktori ke C_INCLUDE_PATH
+export C_INCLUDE_PATH=$XORG_PREFIX/include:$C_INCLUDE_PATH
 
+# Menambahkan direktori ke CPLUS_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$XORG_PREFIX/include:$CPLUS_INCLUDE_PATH
+
+# Set ACLOCAL untuk autoconf
+export ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
+
+# Menyimpan variabel untuk shell
 export PATH PKG_CONFIG_PATH ACLOCAL LIBRARY_PATH C_INCLUDE_PATH CPLUS_INCLUDE_PATH
 EOF
+
 
 source /etc/profile.d/xorg.sh
 echo "$XORG_PREFIX/lib" >> /etc/ld.so.conf
